3ff61b693a1dfe2ff0a2a87734377f1840dfd36a
ForHack <olimjonov-odiljon@mail.ru>
2021-04-28 16:31:23 +0500

    Ready base for projects

---
+++ app/js/index.js
@@ -0,0 +1,8 @@
+// Importing Helpers
+import { NodeHelper,  FuncsHelper }  from './modules/Helpers'
+
+// Getting Methods from Helpers
+const { getNode, getNodes, addClass, removeClass, nextNode } = new NodeHelper()
+const { imgLoadHelper } = new FuncsHelper()
+
+// Invoking Functions from Helper
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+    entry: './app/js/index.js',
+    output: {
+        filename: "bundle.js"
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ]
+};
+
+module.exports = config;
